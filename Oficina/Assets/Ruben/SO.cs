58bae18c564c00c87b7424c85d4df700f7344905
Rubecop <ruben_polo@outlook.com>
2024-01-09 17:35:25 +0100

    ScriptableObject y contador de energia

---
+++ Oficina/Assets/Ruben/SO.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu]
+public class SO : ScriptableObject
+{
+
+    public string name;
+    public float count;
+    public GameObject image;
+
+}

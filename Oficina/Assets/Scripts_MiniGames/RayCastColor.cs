eca7621e171fc38a5637086446ce2b57a76da746
IvanContelles <primonero53@gmail.com>
2024-01-09 19:24:50 +0100

    Minijuego 1 acabado(prototipo)

--- Oficina/Assets/Scripts_MiniGames/RayCastColor.cs
+++ Oficina/Assets/Scripts_MiniGames/RayCastColor.cs
@@ -7,40 +7,45 @@ public class RayCastColor : MonoBehaviour
 {
     public string colo;
     public int rango;
-    
+    public int valorcolor=0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //colo = "Red";
+        //valorcolor = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        RaycastHit hit;
-        Ray ray = new Ray(transform.position, transform.forward);
-        Debug.DrawRay(ray.origin, ray.direction * rango, Color.green);
-        if (Physics.Raycast(ray,out hit))
-        {
-                print("golpea");
-
-            Image image = hit.collider.GetComponent<Image>();
-            colo = image.gameObject.name;
-            print(image.gameObject.name);
-            
-        }
+        
         
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         print(collision.gameObject.name);
-        colo= collision.gameObject.name;
+        if (collision.name == "Orange")
+        {
+            valorcolor = 1;
+            print(valorcolor);
+        }else if (collision.name == "Green")
+        {
+            valorcolor = 2;
+            print(valorcolor);
+        }
     }
+    
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.name == ("Orange"))
+        if (collision.gameObject.name == ("Green"))
         {
-            colo = "Red";
+            valorcolor = 1;
+            print(valorcolor);
+        }
+        if (collision.gameObject.name == ("Orange")&&colo!="Green")
+        {
+            valorcolor = 0;
+            print(valorcolor);
             print("Red");
         }
     }
